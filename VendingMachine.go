457d35e8278d09531a7d57813f96e311c742bd9e
Karl Kroll <kwkroll.32@gmail.com>
2016-03-13 12:34:30 -0400

    wrote test and func to accept coins and add to running total.

--- VendingMachine.go
+++ VendingMachine.go
@@ -1,14 +1,23 @@
 package VendingMachine
 
 // Machine is an class to represent the vending machine
-type Machine struct {}
+type Machine struct {
+    RunningTotal int
+}
 
 // NewMachine is a constructor for a new machine
 func NewMachine() *Machine {
-    return new(Machine)
+    m := new(Machine)
+    m.RunningTotal = 0
+    return m
 }
 
 // ToString will return a string representation of this machine
-func (m Machine) ToString() string {
+func (m *Machine) ToString() string {
     return "a machine"
+}
+
+// AcceptCoins will add the input coin to the running total
+func (m *Machine) AcceptCoins(c int) {
+    m.RunningTotal += c
 }
--- tests/vending_test.go
+++ tests/vending_test.go
@@ -4,12 +4,21 @@ import (
     "GoVending"
     "testing"
     "fmt"
+    "strconv"
 )
 
 func TestSetup(t *testing.T) {
     machine := VendingMachine.NewMachine()
     fmt.Println(machine.ToString())
     
+}
+
+func TestAcceptCoins(t *testing.T) {
+    machine := VendingMachine.NewMachine()
+    machine.AcceptCoins(5)
+    if machine.RunningTotal != 5 {
+        t.Errorf("expecting 5, got " + strconv.Itoa(machine.RunningTotal) )
+    }
 }
 
 func main() {}
