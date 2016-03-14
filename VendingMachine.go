52b49b73997f5716bd6d78f1db4eae427c3aea56
Karl Kroll <kwkroll.32@gmail.com>
2016-03-13 21:20:25 -0400

    reorganized code to begin adding next functionality

--- VendingMachine.go
+++ VendingMachine.go
@@ -4,15 +4,21 @@ import (
     "strconv"
 )
 
-// Machine is an class to represent the vending machine
+/*
+    Machine struct and bound functions 
+*/
+
+// Machine is a class to represent the vending machine
 type Machine struct {
     RunningTotal int
+    InputCoins map[int]int 
 }
 
 // NewMachine is a constructor for a new machine
 func NewMachine() *Machine {
     m := new(Machine)
     m.RunningTotal = 0
+    m.InputCoins = map[int]int{1: 0, 5: 0, 10: 0, 25: 0}
     return m
 }
 
@@ -29,11 +35,17 @@ func (m *Machine) AcceptCoins(c int) error {
     var err error
     if IsValidCoin(c) { 
         m.RunningTotal += c
+        m.InputCoins[c]++
     } else {
         err = errors.New("invalid coin value: " + strconv.Itoa(c))
     }
     return err
 }
+
+
+/*
+    Utility Functions 
+*/
 
 // IsValidCoin will check if the input is of value 1, 5, 10, or 25
 func IsValidCoin(c int) bool {
--- tests/vending_test.go
+++ tests/vending_test.go
@@ -9,23 +9,27 @@ import (
     //"errors"
 )
 
+// initialize a vending machine instance for the tests
 var machine = VendingMachine.NewMachine()
 
 func loadACoin(t *testing.T) func(coinIn int) {
+    // initialize the local total and error object
+    // these are modified in subsequent runs of the closure
     localTotal := 0
     var err error
+    // the closure 
     return func(coinIn int)  {
         err = machine.AcceptCoins(coinIn)
         if err == nil {
             // the machine took the coin fine 
             localTotal += coinIn
-            if machine.RunningTotal != localTotal {
-                // although coin was accepted, the totals dont add up 
-                t.Errorf("expecting " + strconv.Itoa(localTotal) +", got " + strconv.Itoa(machine.RunningTotal) )
-            }
         } else if err != nil {
             // the machine failed to take this coin 
             fmt.Println(err)
+        }
+        if machine.RunningTotal != localTotal {
+            // the totals dont add up 
+            t.Errorf("expecting " + strconv.Itoa(localTotal) +", got " + strconv.Itoa(machine.RunningTotal) )
         }
     }
 }
